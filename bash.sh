ca0b21352295050f58b119c282bde1ad62cdc3f0
ShatunMoto <makcstep@gmail.com>
2024-07-17 12:19:10 +0700

    Add files via upload

--- bash.sh
+++ bash.sh
@@ -15,13 +15,14 @@ sudo apt install docker.io docker-compose #Установить докер
 sudo adduser $USER docker
 
 sudo mkdir ~/docker_image_astra
-sudo ~/./makeastra ~/docker_image_astra
+#sudo ~/./makeastra ~/docker_image_astra
 sudo cp /etc/apt/sources.list.d/sources_last_astra.list ~/docker_image_astra/etc/apt/sources.list
 
 sudo echo "#!/bin/bash
 tar -C \$1 -cpf - . | docker import - \$2 --change \"ENV PATH /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\" --change 'CMD [\"/bin/bash\"]' --change \"ENV LANG=ru_RU.UTF-8\"" &> ~/dockerimport
 sudo chmod +x ~/dockerimport
-sudo ~/./dockerimport ~/docker_image_astra astra:stable-orel
+#sudo ~/./dockerimport ~/docker_image_astra astra:stable-orel
+sudo mkdir ~/docker_image_apache
 sudo echo  "<VirtualHost *:80>
     DocumentRoot /srv/www/wordpress
     <Directory /srv/www/wordpress>
@@ -34,21 +35,22 @@ sudo echo  "<VirtualHost *:80>
         Options FollowSymLinks
         Require all granted
     </Directory>
-</VirtualHost>" &> ~/wordpress.conf
+</VirtualHost>" &> ~/docker_image_apache/wordpress.conf
 
-sudo mkdir ~/docker_image_apache
 #!TODO добавить сюда скачивание и wordpress и настройку apache
 sudo echo "
 FROM astra:stable-orel
+ENV LC_CTYPE=en_US.UTF-8
+ENV LC_ALL=en_US.UTF-8
+ENV LANG=en_US.UTF-8
+ENV LANGUAGE=en:el
 RUN echo \"ru_RU.UTF-8 UTF-8\" >> /etc/locale.gen | echo \"en_US.UTF-8 UTF-8\" >> /etc/locale.gen | locale-gen | update-locale ru_RU.UTF-8
-RUN apt update && apt install -qy apache2 ghostscript libapache2-mod-php php php-bcmath php-curl php-imagick php-intl php-json php-mbstring php-xml php-zip
+RUN apt update && apt install -qy curl apache2 ghostscript libapache2-mod-php php php-bcmath php-curl php-imagick php-intl php-json php-mbstring php-xml php-zip
 RUN adduser --disabled-password --gecos \"\" test && echo test:test | chpasswd
-RUN mkdir -p /srv/www | chown www-data: /srv/www | curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+RUN mkdir -p /srv/www | curl https://wordpress.org/latest.tar.gz | tar zx -C /srv/www | chown -R www-data: /srv/www 
 RUN a2ensite wordpress | a2enmod rewrite | a2dissite 000-default
-RUN sudo -u www-data sed -i 's/database_name_here/wordpress/' /srv/www/wordpress/wp-config.php
-RUN sudo -u www-data sed -i 's/username_here/wordpress/' /srv/www/wordpress/wp-config.php
-RUN sudo -u www-data sed -i 's/password_here/12345678/' /srv/www/wordpress/wp-config.php
-COPY /root/wordpress.conf /etc/apache2/sites-available/
+COPY wordpress.conf /etc/apache2/sites-available/wordpress.conf
+RUN cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php | sed -i 's/database_name_here/wordpress/' /srv/www/wordpress/wp-config.php | sed -i 's/username_here/wordpress/' /srv/www/wordpress/wp-config.php | sed -i 's/password_here/12345678/' /srv/www/wordpress/wp-config.php
 " &> ~/docker_image_apache/Dockerfile
 #RUN mysql -Bse \"CREATE DATABASE wordpress;CREATE USER wordpress@localhost IDENTIFIED BY '12345678';GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost;FLUSH PRIVILEGES;\"
 
